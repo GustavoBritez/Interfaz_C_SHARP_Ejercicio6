f9f71f0d70d9d626d7d66ac5996e45476856f01e
Gustavo Daniel Britez Alvarenga <115171270+GustavoBritez@users.noreply.github.com>
2023-09-03 15:03:23 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Ejercicio_6
+{
+    partial class Ejercicio_6
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BTN_Comenzar = new System.Windows.Forms.Button();
+            this.TX_1 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(636, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desarrollar un programa que permita calcular los primeros n números de la serie d" +
+    "e Fibonacci. El número n es ingresado por el usuario.";
+            // 
+            // BTN_Comenzar
+            // 
+            this.BTN_Comenzar.Location = new System.Drawing.Point(325, 34);
+            this.BTN_Comenzar.Name = "BTN_Comenzar";
+            this.BTN_Comenzar.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Comenzar.TabIndex = 1;
+            this.BTN_Comenzar.Text = "Comenzar";
+            this.BTN_Comenzar.UseVisualStyleBackColor = true;
+            this.BTN_Comenzar.Click += new System.EventHandler(this.BTN_Comenzar_Click_1);
+            // 
+            // TX_1
+            // 
+            this.TX_1.Location = new System.Drawing.Point(78, 74);
+            this.TX_1.Name = "TX_1";
+            this.TX_1.Size = new System.Drawing.Size(599, 20);
+            this.TX_1.TabIndex = 2;
+            this.TX_1.TextChanged += new System.EventHandler(this.TX_1_TextChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Ejercicio_6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(752, 120);
+            this.Controls.Add(this.TX_1);
+            this.Controls.Add(this.BTN_Comenzar);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio_6";
+            this.Text = "Ejercicio 6";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BTN_Comenzar;
+        private System.Windows.Forms.TextBox TX_1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_6
+{
+    public partial class Ejercicio_6 : Form
+    {
+        private List<int> Fibonacci = new List<int>();
+        public Ejercicio_6()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_Comenzar_Click_1(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(Microsoft.VisualBasic.Interaction.InputBox("Ingrese un numero"));
+            while (num < 0)
+            {
+                num = Int32.Parse(Microsoft.VisualBasic.Interaction.InputBox("Ingrese un numero POSITIVO"));
+            }
+           
+            for(int i = 0; i<num; i++)
+            {
+                Fibonacci.Add(Fibo(i));
+            }
+
+            TX_1.Text = string.Join(" ", Fibonacci);
+
+
+        }
+        private int Fibo(int n)
+        {
+            if (n <= 1)
+           {
+                return n;
+           }
+           else
+           {
+                return Fibo(n - 1) + Fibo(n - 2);
+          }
+        }
+
+        private void TX_1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
